b81ebd2e89188b16b9e120308d723a1af8bcda68
Mo Zhu <mozhu888@gmail.com>
2016-01-08 13:57:19 -0500

    Trump and Bernie!

--- hidePolitics/ViewController.swift
+++ hidePolitics/ViewController.swift
@@ -21,5 +21,28 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet weak var berniePic: UIImageView!
+    @IBOutlet weak var trumpPic: UIImageView!
+    @IBOutlet weak var hideBernieButton: UIButton!
+    @IBOutlet weak var hideTrumpButton: UIButton!
+    
+    @IBAction func hideBernie(sender: AnyObject) {
+        if berniePic.hidden {
+            berniePic.hidden = false
+            hideBernieButton.setTitle("Hide Bernie", forState: UIControlState.Normal)
+        } else {
+            berniePic.hidden = true
+            hideBernieButton.setTitle("Show Bernie", forState: UIControlState.Normal)
+        }
+    }
+    @IBAction func hideTrump(sender: AnyObject) {
+        if trumpPic.hidden {
+            trumpPic.hidden = false
+            hideTrumpButton.setTitle("Hide Trump", forState: UIControlState.Normal)
+        } else {
+            trumpPic.hidden = true
+            hideTrumpButton.setTitle("Show Trump", forState: UIControlState.Normal)
+        }
+    }
 }
 
